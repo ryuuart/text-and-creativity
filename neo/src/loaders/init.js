472306e8aa62a4f839c17a0894452c64ddb3a022
18nguyenl <18nguyenl@gmail.com>
2022-03-04 21:02:38 -0600

    Try to get database connection to work

--- neo/src/loaders/init.js
+++ neo/src/loaders/init.js
@@ -1,3 +1,12 @@
-export default async function (app) {
+import Config from "../../util/config.js";
+import fastifyPostgres from "fastify-postgres"
 
+export default async function (app) {
+    await Config();
+
+    app.register(fastifyPostgres, {
+        connectionString: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production" ? true : false,
+        sslmode: process.env.NODE_ENV === "production" ? "require" : "disable",
+    })
 }
--- neo/src/routes/web.js
+++ neo/src/routes/web.js
@@ -1,5 +1,8 @@
 export async function web(fastify, options) {
     fastify.get('/', async (req, rep) => {
-        return "ehj!"
+        const client = await fastify.pg.connect();
+        const { rows } = await client.query('SELECT text FROM words LIMIT 50');
+        client.release();
+        return rows;
     })
 }
