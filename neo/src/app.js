851a6690741c8d01a158720f5f46aad3238af77c
18nguyenl <18nguyenl@gmail.com>
2022-03-04 18:48:17 -0600

    Fix Heroku port issues 2

--- neo/src/app.js
+++ neo/src/app.js
@@ -9,7 +9,7 @@ const start = async () => {
     try {
         Initializer(fastify)
 
-        await fastify.listen(process.env.PORT || 3000)
+        await fastify.listen(process.env.PORT || 3000, "0.0.0.0")
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
